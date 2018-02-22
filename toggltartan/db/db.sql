9cc19ff8145c08b80918455869d567e56786878d
Nijanthan Hariharan <me@nijanthanh.com>
2018-02-21 19:46:18 -0500

    Modify schema of events table

--- toggltartan/db/db.sql
+++ toggltartan/db/db.sql
@@ -18,12 +18,15 @@ create table `events`(
     user_id INT,
     is_active TINYINT DEFAULT 1,
     course_id VARCHAR(10) NULL,
+    name VARCHAR(500) NULL,
     start_time Time,
-    duration INT NULL,
-    frequency VARCHAR(20) NULL, /* Possible values are onetime, weekly, monthly */
+    end_time Time,
+    frequency VARCHAR(20) NULL, /* Possible values are onetime, daily, weekly, monthly */
     from_date Date,
     till_date Date,
     days_of_week INT NULL,
+    date_created DateTime,
+    date_updated DateTime,
     PRIMARY KEY (`id`),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
